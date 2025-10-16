7d61111f4a3d8f29e2809817e1d53edce33289e4
Sveinung <sveinung.rekaa@nenett.no>
2025-10-16 13:33:07 +0200

    Config fil (trekkeplan.cfg)

--- db/connection.py
+++ db/connection.py
@@ -1,16 +1,28 @@
+import configparser
 import mysql.connector
-from config import DB_HOST, DB_USER, DB_PASS, DB_NAME
 
 def get_connection():
 #    print("get_connection start")
     try:
-        connection = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASS,
-        database=DB_NAME
-        )
+        db_config = hent_mysql_config()
+        connection = mysql.connector.connect(**db_config)
 #        print("get_connection slutt")
         return connection
     except Exception as e:
         print("Feil: {e}")
+
+
+def hent_mysql_config(filnavn="trekkeplan.cfg"):
+    config = configparser.ConfigParser()
+    config.read(filnavn)
+
+    if "mysql" not in config:
+        raise ValueError("Mangler [mysql]-seksjon i config-filen")
+
+    return {
+        "host": config["mysql"].get("host", "localhost"),
+        "port": config["mysql"].getint("port", 3306),
+        "user": config["mysql"]["user"],
+        "password": config["mysql"]["password"],
+        "database": config["mysql"]["database"],
+    }
